1972b699f97c89055681d9ac485171fa24e7a878
moonhyunhu <ansgusgn@gmaile.com>
2024-06-14 12:52:18 +0900

    2

--- src/init/socket.js
+++ src/init/socket.js
@@ -2,7 +2,14 @@ import { Server as SocketIO } from 'socket.io';
 import registerHandler from '../handlers/register.handler.js';
 
 const initSocket = (server) => {
-  const io = new SocketIO();
+  const io = new SocketIO({
+    cors: {
+      origin: 'http://15.164.171.110:5001', // 허용할 클라이언트 도메인
+      methods: ['GET', 'POST'],
+      allowedHeaders: ['my-custom-header'],
+      credentials: true,
+    },
+  });
   io.attach(server);
 
   registerHandler(io);
